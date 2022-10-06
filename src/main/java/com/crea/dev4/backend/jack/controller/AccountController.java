b35f09c93865aa54ce32bad20c2a71a8e0923362
Cocosmos <mipam.guillot@crea-inseec.com>
2022-10-06 23:17:40 +0200

    ✅ Create login and signup

--- src/main/java/com/crea/dev4/backend/jack/controller/AccountController.java
+++ src/main/java/com/crea/dev4/backend/jack/controller/AccountController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crea.dev4.backend.jack.dao.AccountDao;
@@ -25,6 +27,25 @@ public class AccountController {
     @GetMapping(value = "/accounts")
     public List<Account> listAccounts() {
         return AccountDao.findAll();
+    }
+
+    @PostMapping(value = "/account/create")
+    public Account createAccount(@RequestBody Account a) {
+        AccountDao.save(a);
+        return a;
+    }
+
+    @PostMapping(value = "/account/login")
+    public boolean loginAccount(@RequestBody Account a) {
+        boolean auth = false;
+        Account AccountFinded = AccountDao.findByEmailLike(a.getEmail());
+        if (AccountFinded != null) {
+            if (AccountFinded.getPassword().equals(a.getPassword())) {
+                auth = true;
+            }
+        }
+
+        return auth;
     }
 
 }
