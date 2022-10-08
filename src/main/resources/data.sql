1b36c22a41d2a2e33c10b2368b2b48950ba82f83
cocosmos <mipam.guillot@crea-inseec.com>
2022-10-08 18:21:29 +0200

    ✅ Favorite

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,9 @@ INSERT INTO product(id, name, category, price, bitcoin, ethereum) VALUES('3','As
 INSERT INTO account(id, email, name, password) VALUES('1','john@example.com','John','test');
 INSERT INTO account(id, email, name, password) VALUES('2','joe@example.com','Joe','test');
 INSERT INTO account(id, email, name, password) VALUES('3','jack@example.com','Jack','test');  
+
+INSERT INTO favorite(accountid, productid) VALUES('3','1');
+INSERT INTO favorite(accountid, productid) VALUES('3','2');  
+INSERT INTO favorite(accountid, productid) VALUES('3','3');  
+INSERT INTO favorite(accountid, productid) VALUES('2','2');  
+INSERT INTO favorite(accountid, productid) VALUES('2','3');  
